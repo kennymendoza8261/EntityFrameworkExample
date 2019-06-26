d10e11351bd524d7320881ee4bee91b38a96b51b
Unknown <19vign@elmbrookstudents.org>
2019-06-26 14:52:29 -0500

    Made Duplicate Barrels Functionality

---
+++ EntityFrameworkExample/Models/NonEntity/CubeCount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EntityFrameworkExample.Models
+{
+    public class CubeCount
+    {
+        public double Radius { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string ConstructionMaterial { get; set; }
+        public string Contents { get; set; }
+        public string CurrentLocation { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int Amount { get; set; }
+    }
+}
